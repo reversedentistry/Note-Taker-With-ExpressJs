1a5d3f22e0bcf74a107938bb8308ac6b812f26d9
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-17 16:50:58 -0700

    Added port

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express"); 
 const app = express(); 
+const PORT = process.env.PORT || 3001;
 
 const htmlRouter = require("./routes/html"); 
 const apiRouter = require("./routes/api"); 
 
-app.use(express.static("public")); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
 app.use("*", htmlRouter); 
 app.use("/api", apiRouter); 
+
+app.use(express.static("public")); 
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
 
 
  
