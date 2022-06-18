e7da4ef4606e457ede88ea80ad62eb6abefa4c75
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-17 23:24:25 -0700

    Added function to read notes json file and add new note

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,23 @@ const router = require("express").Router();
 const notes = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");
 // const path = require("path");
+const fs = require("fs"); 
 
 router.get("/notes"), (req, res) => res.json(notes);
 
 router.post("/notes", (req, res) => {
     req.body.id = uuidv4(); 
+    console.log(req.body); 
+    const newNote = req.body; 
+    fs.readFile("../../db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err); 
+        } else {
+            const parsedNotes = JSON.parse(data);  
+            parsedNotes.push(newNote); 
+            fs.writeFileSync("../db/db.json", parsedNotes);
+        }
+    })
 }); 
 
 module.exports = router; 
