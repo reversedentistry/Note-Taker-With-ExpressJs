66a089a0ea6e0b8a935692d467a557d5a70d68a8
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-18 09:46:06 -0700

    Corrected file paths within fs functions

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,27 @@
 const router = require("express").Router(); 
 const notes = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");
-// const path = require("path");
+const path = require("path");
 const fs = require("fs"); 
 
-router.get("/notes"), (req, res) => res.json(notes);
-
+router.get("/api/notes"), (req, res) => {
+    res.json(notes);
+}; 
 router.post("/notes", (req, res) => {
+    console.info(`${req.method} request received to add a note`); 
     req.body.id = uuidv4(); 
     console.log(req.body); 
     const newNote = req.body; 
-    fs.readFile("../../db/db.json", "utf8", (err, data) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) {
             console.error(err); 
         } else {
             const parsedNotes = JSON.parse(data);  
             parsedNotes.push(newNote); 
-            fs.writeFileSync("../db/db.json", parsedNotes);
+            fs.writeFile("./db/db.json", JSON.stringify(parsedNotes, null, 4), (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!'));
         }
     })
 }); 
